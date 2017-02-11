90c80d2c66da58f6568c2e90b4ea25a53ddc8965
陈宝仪 <chen.bao.yi@qq.com>
2017-02-11 11:11:51 -0600

    Update CountingBitSort.java

--- CountingBitSort.java
+++ CountingBitSort.java
@@ -9,28 +9,28 @@ public class CountingBitSort {
     private static final int BITS = 6;
     private static final int MASK = 1 << 6;
 
-    public CountingBitSort(int max) {
-        int x = max >> BITS;
-        int y = max & (MASK - 1);
+    public CountingBitSort(int maxNum) {
+        int x = maxNum >> BITS;
+        int y = maxNum & (MASK - 1);
         if (y > 0) x = x + 1;
         this.ary = new long[x];
     }
 
     /**
-     * @param i 0->Integer.MAX_VALUE
+     * @param num 0->Integer.MAX_VALUE
      */
-    public void sort(int i) {
-        int x = i >> BITS;
-        int y = i & (MASK - 1);
+    public void sort(int num) {
+        int x = num >> BITS;
+        int y = num & (MASK - 1);
         ary[x] |= (1L << y);
     }
 
     /**
-     * @param i 0->Integer.MAX_VALUE
+     * @param num 0->Integer.MAX_VALUE
      */
-    public boolean contains(int i) {
-        int x = i >> BITS;
-        int y = i & (MASK - 1);
+    public boolean contains(int num) {
+        int x = num >> BITS;
+        int y = num & (MASK - 1);
         return ((ary[x] >>> y) & 1) != 0;
     }
 
@@ -54,9 +54,9 @@ public class CountingBitSort {
     }
 
     /**
-     * getIndex之前的预处理
+     * getIndex和top k之前的预处理
      */
-    public void postSort() {
+    public void preProcessing() {
         this.count = new int[ary.length + 1];
         int total = 0;
         for (int idx = 1; idx < count.length; idx++) {
@@ -66,11 +66,11 @@ public class CountingBitSort {
     }
 
     /**
-     * @param i 0->Integer.MAX_VALUE
+     * @param num 0->Integer.MAX_VALUE
      */
-    public int getIndex(int i) {
-        int x = i >> BITS;
-        int y = i & (MASK - 1);
+    public int getIndex(int num) {
+        int x = num >> BITS;
+        int y = num & (MASK - 1);
         if (((ary[x] >>> y) & 1) == 0) return -1;
         if (y == 0) return count[x];
         return count[x] + Long.bitCount(ary[x] << (MASK - y) >>> (MASK - y));
@@ -83,25 +83,22 @@ public class CountingBitSort {
     public static void main(String[] args) {
         long et, st = System.currentTimeMillis();
         CountingBitSort cb = new CountingBitSort(Integer.MAX_VALUE);
-        for (int i = Integer.MAX_VALUE; i >= 0; i--) {
-            cb.sort(i);
-        }
+        for (int i = Integer.MAX_VALUE; i >= 0; i--) cb.sort(i);
         et = System.currentTimeMillis();
         System.out.println("sort:" + (et - st) / 1000d);
+
+        cb.preProcessing();
+
         st = System.currentTimeMillis();
-        cb.postSort();
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            if (i != cb.getIndex(i)) {
-                System.out.println("error" + i);
-            }
+            if (i != cb.getIndex(i)) System.out.println("error" + i);
         }
         et = System.currentTimeMillis();
         System.out.println("sort checking:" + (et - st) / 1000d);
+
         st = System.currentTimeMillis();
         int[] r = cb.top(65);
-        for (int i : r) {
-            System.out.println(i);
-        }
+        for (int i : r) System.out.println(i);
         et = System.currentTimeMillis();
         System.out.println("top k:" + (et - st) / 1000d);
     }
